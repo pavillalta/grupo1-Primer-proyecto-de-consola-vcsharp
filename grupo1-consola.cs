7c4d904f8ae9a6dff00b9c1b555ae50644db224b
Pedro Villalta <pavblogs@gmail.com>
2021-07-30 11:32:20 -0600

    Agregar archivos de proyecto.

---
+++ grupo1-consola.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace grupo1_Primer_proyecto_de_consola_vcsharp
+{
+    class semana1
+    {
+        static void Main(string[] args)
+        {
+
+            //Uso variables en visual C#
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+
+        }
+    }
+}
